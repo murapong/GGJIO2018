7edc0ae6e2428f69896211add43829c39a5ef80d
murapong <murapong@gmail.com>
2018-01-27 11:16:14 +0900

    Add Title scene

---
+++ client/GGJIO2018/Assets/Scripts/Title/TitleSceneController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+public class TitleSceneController : MonoBehaviour
+{
+    #region enum
+
+    #endregion
+
+    #region const
+
+    #endregion
+
+    #region public property
+
+    #endregion
+
+    #region private property
+
+    /// <summary>
+    /// The start button.
+    /// </summary>
+    [SerializeField]
+    Button startButton;
+
+    #endregion
+
+    #region public method
+
+    #endregion
+
+    #region private method
+
+    #endregion
+
+    #region event
+
+    void Start()
+    {
+        startButton.onClick.AsObservable().First()
+            .Subscribe(_ => Debug.Log("Go to Game."));
+    }
+
+    void Update()
+    {
+
+    }
+
+    #endregion
+}
