21c8b48e0ff004bc60721f0005c9a31bfccc3943
murapong <murapong@gmail.com>
2018-01-27 19:43:00 +0900

    Delete title scene

--- client/GGJIO2018/Assets/Scripts/Config/Scene.cs
+++ client/GGJIO2018/Assets/Scripts/Config/Scene.cs
@@ -1,11 +1,6 @@
 ﻿public class Scene
 {
     /// <summary>
-    /// The title scene.
-    /// </summary>
-    public const string Title = "Title";
-
-    /// <summary>
     /// The game scene..
     /// </summary>
     public const string Game = "Game";
