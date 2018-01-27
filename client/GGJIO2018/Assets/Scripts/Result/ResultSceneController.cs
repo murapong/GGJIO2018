da7b60aa8574abe6bb0171934959e143f9c4291a
murapong <murapong@gmail.com>
2018-01-27 11:43:52 +0900

    Add Result scene

---
+++ client/GGJIO2018/Assets/Scripts/Result/ResultSceneController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+using UniRx;
+
+public class ResultSceneController : MonoBehaviour
+{
+    #region enum
+
+    #endregion
+
+    #region const
+
+    #endregion
+
+    #region public property
+
+    /// <summary>
+    /// The retry button.
+    /// </summary>
+    [SerializeField]
+    Button retryButton;
+
+    #endregion
+
+    #region private property
+
+    #endregion
+
+    #region public method
+
+    #endregion
+
+    #region private method
+
+    #endregion
+
+    #region event
+
+    void Start()
+    {
+        retryButton.onClick.AsObservable().First()
+            .Subscribe(_ => SceneManager.LoadScene(Scene.Game));
+    }
+
+    void Update()
+    {
+
+    }
+
+    #endregion
+}
